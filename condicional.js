f41656e3a5fcedd8e2b30c7dc0f230aab601198a
Gabriel Expindola <gabriel.madeira@edu.senai.br>
2026-09-15 16:18:21 -0300

    :construction: mais alguns exercicios feitos

--- condicional.js
+++ condicional.js
@@ -73,13 +73,58 @@ if (numero3 % 2 === 0) {
 
 // Calculadora Simples
 
+let numerocalculadora1 = Number(prompt("Digite o primeiro número: "));
+let numerocalculadora2 = Number(prompt("Digite o segundo número: "));
+let operacao = prompt("Digite a operação (+, -, *, /): ");
+
+let resultado;
+
+if (operacao === "+") {
+    resultado = numerocalculadora1 + numerocalculadora2;
+} else if (operacao === "-") {
+    resultado = numerocalculadora1 - numerocalculadora2;
+} else if (operacao === "*") {
+    resultado = numerocalculadora1 * numerocalculadora2;
+} else if (operacao === "/") {
+    resultado = numerocalculadora1 / numerocalculadora2;
+} else {
+    console.log("Operação inválida.");
+}
+
 // Maior entre três numeros
 
+
+let numeroescolha1 = Number(prompt("Digite o primeiro número: "));
+let numeroescolha2 = Number(prompt("Digite o segundo número: "));
+let numeroescolha3 = Number(prompt("Digite o terceiro número: "));
+
+if (numeroescolha1 > numeroescolha2 && numeroescolha1 > numeroescolha3)
+{
+    console.log(`Maior número escolhido é o primeiro número: ${numeroescolha1}`);
+}
+if (numeroescolha2 > numeroescolha1 && numeroescolha2 > numeroescolha3) {
+    console.log(`Maior número escolhido é o segundo número: ${numeroescolha2}`);
+}
+if (numeroescolha3 > numeroescolha1 && numeroescolha3 > numeroescolha2) {
+    console.log(`Maior número escolhido é o terceiro número: ${numeroescolha3}`);
+}
+else {
+    console.log("Nenhum dos números é maior que o outro.");
+}
 // Desconto em compras
+
+
 
 // Sistema de login simples
 
+let usuario = prompt("Digite o nome de usuário: ");
+let senha = Number(prompt("Digite a senha: "));
 
+if(usuario == "admin" && senha == 1234) {
+    console.log("Login bem-sucedido.")
+} else {
+    console.log("Acesso negado.")
+}
 
 // Nível Avançado
 
@@ -87,7 +132,39 @@ if (numero3 % 2 === 0) {
 
 // Acesso por idade e convite
 
+let idadeacesso = Number(prompt("Digite sua idade: "));
+let temconvite = prompt("Possui convite? (sim / não)");
+
+if (idadeacesso >= 18 || temconvite == "sim") {
+    console.log("Pode entrar");
+}
+else {
+    console.log("Barrado");
+}
+
+
 // Conversão de notas pra conceitos
+
+let notaconceito = Number(prompt("Insira a nota: "));
+
+if (notaconceito >= 90 && notaconceito <= 100) {
+    console.log("A");
+}
+if (notaconceito >= 80 && notaconceito <= 89) {
+    console.log("B");
+}
+if (notaconceito >= 70 && notaconceito <= 79) {
+    console.log("C");
+}
+if (notaconceito >= 60 && notaconceito <= 69) {
+    console.log("D");
+}
+if (notaconceito < 60) {
+    console.log("F");
+}
+else {
+    console.log("Nota inválida")
+}
 
 // Cálculo de IMC
 
