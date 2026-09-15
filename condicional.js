3078526a53888ffc2a04e6cc05e68b1c2e5b7c68
Gabriel Expindola <gabriel.madeira@edu.senai.br>
2026-09-15 14:38:36 -0300

    :construction: nível básico repeticao e condicional concluido

--- condicional.js
+++ condicional.js
@@ -2,17 +2,71 @@
 
 // Verificar maioridade
 
+let idade = Number(prompt("Digite sua idade: "));
+
+if (idade >= 18) {
+    console.log("Você é maior de idade");
+} else {
+    console.log("Você é menor de idade");
+}
+
 // Verificar se um número é positivo ou negativo
+
+let numero = Number(prompt("Digite um número: "));
+
+if (numero > 0) {
+    console.log("O número é positivo");
+} else if (numero < 0) {
+    console.log("O número é negativo");
+}
 
 // Aprovação de uma prova
 
+let nota = Number(prompt("Digite a nota da prova: "));
+
+if (nota >= 60) {
+    console.log("Aprovado");
+} else {
+    console.log("Reprovado");
+}
+
 // Verificar se um número é positivo, negativo ou zero
+
+let numero2 = Number(prompt("Digite um número: "));
+
+if (numero2 > 0) {
+    console.log("O número é positivo");
+} else if (numero2 < 0) {
+    console.log("O número é negativo");
+} else {
+    console.log("O número é zero");
+}
 
 // Classificação da Idade
 
+let idade2 = Number(prompt("Digite sua idade: "));
+
+if (idade2 >= 0 && idade2 <= 12) {
+    console.log("Você é criança");
+} 
+if (idade2 >= 13 && idade2 <= 17) {
+    console.log("Você é adolescente");
+} else if (idadec >= 18) {
+    console.log("Você é adulto");
+}
+else {
+    console.log("Idade inválida");
+}
+
 // Verificar se um número é par ou impar
 
+let numero3 = Number(prompt("Digite o número a ser verificado: "));
 
+if (numero3 % 2 === 0) {
+    console.log("O número é par");
+} else {
+    console.log("O número é ímpar");
+}
 
 
 // Nível Intermediário
@@ -37,4 +91,4 @@
 
 // Cálculo de IMC
 
-// Verificação de Estoque 
+// Verificação de Estoque
